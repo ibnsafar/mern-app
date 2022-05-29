d15cca2d1e8b04eaac9394e4eb4fcdc09df1253e
Yokub1 <yokubibnsafar@gmail.com>
2022-05-29 11:19:52 +0500

    updating components

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,10 @@ import Navbar from "../components/navbar/Navbar";
 import Header from "../components/header/Header";
 import Featured from "../components/featured/Featured";
 import "./css/home/home.css";
+import ListProperty from "../components/listProperty/ListProperty";
+import FeaturedProperties from "../components/featuredProperties/FeaturedProperties";
+import MailList from "../components/mailList/MailList";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
     return (
@@ -12,6 +16,11 @@ const Home = () => {
                 <div className="homeContainer">
                     <Featured/>
                     <h1 className="homeTitle">Browser by property type</h1>
+                    <ListProperty/>
+                    <h1 className="homeTitle">Home guests love</h1>
+                    <FeaturedProperties/>
+                    <MailList/>
+                    <Footer/>
                 </div>
             </div>
         </>
--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/navbar/Navbar";
 import Header from "../components/header/Header";
+import "./css/List/list.css";
 
 const List = () => {
     return (
